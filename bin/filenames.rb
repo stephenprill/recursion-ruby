69aee10dc0f5769c9bc145917a7bffc50e7fc642
Jeff Dean <jeff@galvanize.it>
2015-03-06 09:27:43 -0700

    remove solutions from master

--- bin/filenames.rb
+++
@@ -1,8 +0,0 @@
-def filenames(pattern, level = 0)
-  Dir.glob(pattern) do |path|
-    puts "#{" " * level * 2}#{File.basename(path)}"
-    filenames(path + '/*', level + 1) if File.directory?(path)
-  end
-end
-
-filenames("*")
